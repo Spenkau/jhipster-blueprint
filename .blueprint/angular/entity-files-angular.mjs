5a0c65f1b924df629c347f0843a130bce3b63068
alexander <kholyavskij@mail.ru>
2024-02-13 20:29:50 +0300

    add code for detecting entity files from source generator-jhipster and delete unused files

--- .blueprint/angular/entity-files-angular.mjs
+++ .blueprint/angular/entity-files-angular.mjs
@@ -2,38 +2,15 @@ import { clientApplicationTemplatesBlock } from '../client/support/files.mjs';
 export const angularFiles = {
   client: [
     {
-      ...clientApplicationTemplatesBlock(),
-      templates: ['entities/_entityFolder_/_entityFile_.model.ts', 'entities/_entityFolder_/_entityFile_.test-samples.ts'],
-    },
-    {
       condition: generator => !generator.embedded,
       ...clientApplicationTemplatesBlock(),
       templates: [
         'entities/_entityFolder_/_entityFile_.routes.ts',
         'entities/_entityFolder_/detail/_entityFile_-detail.component.html',
         'entities/_entityFolder_/detail/_entityFile_-detail.component.ts',
-        'entities/_entityFolder_/detail/_entityFile_-detail.component.spec.ts',
         'entities/_entityFolder_/list/_entityFile_.component.html',
         'entities/_entityFolder_/list/_entityFile_.component.ts',
-        'entities/_entityFolder_/list/_entityFile_.component.spec.ts',
-        'entities/_entityFolder_/route/_entityFile_-routing-resolve.service.ts',
-        'entities/_entityFolder_/route/_entityFile_-routing-resolve.service.spec.ts',
         'entities/_entityFolder_/service/_entityFile_.service.ts',
-        'entities/_entityFolder_/service/_entityFile_.service.spec.ts',
-      ],
-    },
-    {
-      condition: generator => !generator.readOnly && !generator.embedded,
-      ...clientApplicationTemplatesBlock(),
-      templates: [
-        'entities/_entityFolder_/update/_entityFile_-form.service.ts',
-        'entities/_entityFolder_/update/_entityFile_-form.service.spec.ts',
-        'entities/_entityFolder_/update/_entityFile_-update.component.html',
-        'entities/_entityFolder_/update/_entityFile_-update.component.spec.ts',
-        'entities/_entityFolder_/delete/_entityFile_-delete-dialog.component.html',
-        'entities/_entityFolder_/update/_entityFile_-update.component.ts',
-        'entities/_entityFolder_/delete/_entityFile_-delete-dialog.component.ts',
-        'entities/_entityFolder_/delete/_entityFile_-delete-dialog.component.spec.ts',
       ],
     },
   ],
