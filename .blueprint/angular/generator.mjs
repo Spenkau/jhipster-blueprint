37a96fc77efcbcc9d8888768163c761717cea951
alexander <kholyavskij@mail.ru>
2024-02-13 11:27:48 +0300

    add files from generator-jhipster
    Current problem: scope. Variables in .ejs are not connected to any context

--- .blueprint/angular/generator.mjs
+++ .blueprint/angular/generator.mjs
@@ -1,4 +1,5 @@
 import BaseApplicationGenerator from 'generator-jhipster/generators/base-application';
+import { writeFiles } from './files-angular.mjs';
 
 export default class extends BaseApplicationGenerator {
   constructor(args, opts, features) {
@@ -6,16 +7,30 @@ export default class extends BaseApplicationGenerator {
   }
 
   get [BaseApplicationGenerator.WRITING]() {
-    return this.asWritingTaskGroup({
+    const files = this.asWritingTaskGroup({
+      ...super.writing,
+      writeFiles,
       async writingTemplateTask({ application }) {
-        await this.writeFiles({
-          sections: {
-            files: [{ templates: ['template-file-angular'] }],
+        this.fs.copyTpl(
+          this.templatePath('src/main/webapp/app/entities/_entityFolder/list/_entityFile.component.html.ejs'),
+          this.destinationPath('src/main/webapp/app/entities/_entityFolder/list/_entityFile.component.html.ts'),
+          {
+            entityClass: this.props.entityClass,
           },
-          context: application,
-        });
+        );
       },
     });
+    // return this.asWritingTaskGroup({
+    //   async writingTemplateTask({ application }) {
+    //     await this.writeFiles({
+    //       sections: {
+    //         files: [{ templates: ['template-file-angular'] }],
+    //       },
+    //       context: application,
+    //     });
+    //   },
+    // });
+    return files;
   }
 
   get [BaseApplicationGenerator.WRITING_ENTITIES]() {
